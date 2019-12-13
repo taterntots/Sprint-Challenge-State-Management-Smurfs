041f5cb0375592e930ec079963923a6dfa2da838
taterntots <matthew.a.weidner@gmail.com>
2019-12-13 11:19:41 -0500

    created my store and a basic reducer with initial state and a default return

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+    smurfs: [],
+    isFetching: true,
+    error: ''
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reducer;
