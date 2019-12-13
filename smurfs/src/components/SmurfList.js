d0b5b21ea625a6022111aa63b9eb27bd396b5bc2
taterntots <matthew.a.weidner@gmail.com>
2019-12-13 13:28:51 -0500

    added a teeny bit of styling and changed displaying my data from a button click to being displayed right away thanks to useEffect

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { getSmurfs, addSmurfs } from '../actions'
 import SmurfForm from './SmurfForm';
+import styled from 'styled-components';
+
+const GridStyle = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-auto-rows: auto;
+  grid-gap: 20px;
+  grid-auto-flow: row;
+  justify-content: center;
+  align-items: start;
+  margin: 2rem 2rem;
+  background-color: slategrey;
+  border-radius: 15px;
+`
 
 const SmurfList = (props) => {
     // console.log(props);
+
+    useEffect(() => {
+        props.getSmurfs();
+    }, [])
+
     return (
         <div>
-            {props.smurfs.map(s =>
-                <div key={s.id}>
-                    <p>{s.name}</p>
-                    <p>Age: {s.age}</p>
-                    <p>Height: {s.height}</p>
-                </div>
-            )}
-            <button onClick={() => props.getSmurfs()}>Get Smurfs</button>
+            {/* <button onClick={() => props.getSmurfs()}>Get Smurfs</button> */}
             <SmurfForm addSmurfs={props.addSmurfs} />
+            <GridStyle>
+                {props.smurfs.map(s =>
+                    <div key={s.id}>
+                        <p>{s.name}</p>
+                        <p>Age: {s.age}</p>
+                        <p>Height: {s.height}</p>
+                    </div>
+                )}
+            </GridStyle>
         </div>
     )
 }
