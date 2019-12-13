bc8287c9bca3fe5bfe47e532a16c6aecd30f3e95
taterntots <matthew.a.weidner@gmail.com>
2019-12-13 12:24:48 -0500

    created my smurf form for adding smurfs. still needs the logic.

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const SmurfForm = (props) => {
+    console.log(props);
+    const [newSmurf, setNewSmurf] = useState({
+        name: '',
+        age: '',
+        height: '',
+        id: Date.now()
+    })
+
+    const handleChanges = (event) => {
+        setNewSmurf({ ...newSmurf, [event.target.name]: event.target.value })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.addSmurf(newSmurf);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input name='name' type='text' placeholder='name' onChange={handleChanges} />
+                <input age='age' type='text' placeholder='age' onChange={handleChanges} />
+                <input height='height' type='text' placeholder='height' onChange={handleChanges} />
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SmurfForm;
--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { getSmurfs } from '../actions'
+import SmurfForm from './SmurfForm';
 
 const SmurfList = (props) => {
-    console.log(props);
+    // console.log(props);
     return (
         <div>
             {props.smurfs.map(s =>
                 <div key={s.id}>
                     <p>{s.name}</p>
+                    <p>Age: {s.age}</p>
+                    <p>Height: {s.height}</p>
                 </div>
             )}
             <button onClick={() => props.getSmurfs()}>Get Smurfs</button>
+            <SmurfForm />
         </div>
     )
 }
